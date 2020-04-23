7be7c6bf385a8158846d7572c3bc9842afe7d060
jiseedev <jc.vinceedev@gmail.com>
2020-04-23 20:24:00 +0800

    Added view and template and styling for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='home')
+    path('', views.post_list, name='home'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail')
 ]
